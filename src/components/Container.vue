a5fb478cdf9bfbf464549188ed7afc9fc5c6ecf4
seoyeonchoi <choi.seoyeon@kt.com>
2024-03-04 15:30:16 +0900

    create git repository vuestagram

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <Post :insta="insta" v-for="(insta, i) in InstaData" :key=" i"/>
+</div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    name: 'Container-compo',
+    components: {
+        Post
+    },
+    props: {
+        InstaData: Array,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
